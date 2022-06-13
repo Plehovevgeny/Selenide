28b728cca85e2ae4eb449cf351eaa293fe2d2953
Plehov Evgeny <plehov1992@yandex.ru>
2022-06-13 17:14:12 +0500

    add more tests for task2

--- src/test/java/OrderingACardSelenide.java
+++ src/test/java/OrderingACardSelenide.java
@@ -9,12 +9,61 @@ import static com.codeborne.selenide.files.DownloadActions.click;
 public class OrderingACardSelenide {
 
     @Test
-    public void shouldOrderingACard(){
+    public void shouldOrderingACardPositive(){
         open("http://localhost:9999/");
         $("[data-test-id = name] input").setValue("Евгений");
         $("[data-test-id = phone] input").setValue("+79221956008");
         $("[data-test-id = agreement]").click();
         $("[type = button]").click();
         $("[data-test-id = order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    public void shouldOrderingACardWithEnglishWordsInName(){
+        open("http://localhost:9999/");
+        $("[data-test-id = name] input").setValue("John");
+        $("[data-test-id = phone] input").setValue("+79221956008");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $("[data-test-id = name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    public void shouldOrderingACardWithNoName(){
+        open("http://localhost:9999/");
+        $("[data-test-id = name] input").setValue("");
+        $("[data-test-id = phone] input").setValue("+79221956008");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $("[data-test-id = name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldOrderingACardNoPhone(){
+        open("http://localhost:9999/");
+        $("[data-test-id = name] input").setValue("Евгений");
+        $("[data-test-id = phone] input").setValue("");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $("[data-test-id = phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldOrderingACardBadPhone(){
+        open("http://localhost:9999/");
+        $("[data-test-id = name] input").setValue("Евгений");
+        $("[data-test-id = phone] input").setValue("79221956008");
+        $("[data-test-id = agreement]").click();
+        $("[type = button]").click();
+        $("[data-test-id = phone].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    public void shouldOrderingACardNoCheckbox(){
+        open("http://localhost:9999/");
+        $("[data-test-id = name] input").setValue("Евгений");
+        $("[data-test-id = phone] input").setValue("+79221956008");
+        $("[type = button]").click();
+        $("[data-test-id = agreement].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 }
