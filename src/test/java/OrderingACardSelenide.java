36c1275cc68454dc3f897331fe36236f09258370
Plehov Evgeny <plehov1992@yandex.ru>
2022-06-13 14:31:22 +0500

    change junit dependency

--- src/test/java/OrderingACardSelenide.java
+++ src/test/java/OrderingACardSelenide.java
@@ -5,13 +5,12 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.files.DownloadActions.click;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderingACardSelenide {
 
     @Test
     public void shouldOrderingACard(){
-        open("http://localhost:9999");
+        open("http://localhost:9999/");
         SelenideElement form = $("[data-test-id=callback-form]");
         form.$("[data-test-id = name] input").setValue("Евгений");
         form.$("[data-test-id = phone] input").setValue("+79221956008");
